60eef485bffb1e8d3beb7ab2ef6f459f52653c9a
handsometree1 <1203969080@qq.com>
2020-04-18 08:03:25 +0800

    anxios

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,75 +1,53 @@
 <template>
-    <div class="login">
+  <div class="login">
+    <el-container>
+      <el-header>欢迎登陆后台管理系统</el-header>
+      <el-main>
         <el-form ref="form" :model="form" label-width="80px">
-  <el-form-item label="活动名称">
-    <el-input v-model="form.name"></el-input>
-  </el-form-item>
-  <el-form-item label="活动区域">
-    <el-select v-model="form.region" placeholder="请选择活动区域">
-      <el-option label="区域一" value="shanghai"></el-option>
-      <el-option label="区域二" value="beijing"></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item label="活动时间">
-    <el-col :span="11">
-      <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-    </el-col>
-    <el-col class="line" :span="2">-</el-col>
-    <el-col :span="11">
-      <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-    </el-col>
-  </el-form-item>
-  <el-form-item label="即时配送">
-    <el-switch v-model="form.delivery"></el-switch>
-  </el-form-item>
-  <el-form-item label="活动性质">
-    <el-checkbox-group v-model="form.type">
-      <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-      <el-checkbox label="地推活动" name="type"></el-checkbox>
-      <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-      <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-    </el-checkbox-group>
-  </el-form-item>
-  <el-form-item label="特殊资源">
-    <el-radio-group v-model="form.resource">
-      <el-radio label="线上品牌商赞助"></el-radio>
-      <el-radio label="线下场地免费"></el-radio>
-    </el-radio-group>
-  </el-form-item>
-  <el-form-item label="活动形式">
-    <el-input type="textarea" v-model="form.desc"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="onSubmit">立即创建</el-button>
-    <el-button>取消</el-button>
-  </el-form-item>
-</el-form>
-    </div>
+          <i class="el-icon-user-solid"></i>
+          <el-form-item class="el-icon-user-solid" label="用户名">
+            <el-input v-model="form.name" placeholder="请输入用户名"></el-input>
+          </el-form-item>
+          <br>
+          <i class="el-icon-unlock"></i>
+          <el-form-item class="el-icon-unlock" label="密码">
+            <el-input v-model="form.pass" placeholder="请输入密码" show-password></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-main>
+      <el-footer></el-footer>
+    </el-container>
+  </div>
 </template>
 <script>
 export default {
   name: "Fine",
-   data() {
-      return {
-        form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        }
+  data() {
+    return {
+      form: {
+        name: "",
+        pass: ""
       }
-    },
-    methods: {
-      onSubmit() {
-        console.log('submit!');
-      }
+    };
+  },
+  methods: {
+    onSubmit() {
+        this.$ajax.get('/testTime').then(function (res){
+            var result = res;
+            debugger;
+            console.log(res.data)
+        },function(res){
+            debugger;
+            var result = res;
+            console.log("error:"+res)
+        })
+      console.log("submit!");
     }
+  }
 };
 </script>
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -6,9 +6,28 @@ import router from './router'
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
+import axios from 'axios'
+
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+Vue.prototype.$ajax = axios
+
+// axios 配置
+
+axios.defaults.timeout = 5000;
+
+//axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8';
+
+// axios.defaults.headers.post['Content-Type'] = 'json';
+
+// axios.defaults.baseURL = 'http://localhost:8080/项目名/';
+
+axios.defaults.baseURL = 'http://106.12.17.163:5560/';
+
 
 /* eslint-disable no-new */
 new Vue({
